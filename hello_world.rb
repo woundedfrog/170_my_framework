9cd3283a82e21ea83c874752c27f5cb579f534e0
Bernard Maisel <ntjust26@gmail.com>
2018-06-07 19:02:54 +0900

    updated hello_world call method
    Added HTML to hello_world file.

--- hello_world.rb
+++ hello_world.rb
@@ -5,15 +5,23 @@ class HelloWorld
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', {"Content-Type" => 'text/plain'}, ["Hello World!"]]
+      [
+        '200',
+        {"Content-Type" => 'text/html'},
+        ["<html><body><h2>Hello World!</h2></body></html>"]
+      ]
     when '/advice'
-      piece_of_advice = Advice.new.generate    # random piece of advice
-      ['200', {"Content-Type" => 'text/plain'}, [piece_of_advice]]
+      piece_of_advice = Advice.new.generate
+      [
+        '200',
+        {"Content-Type" => 'text/html'},
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
     else
       [
         '404',
-        {"Content-Type" => 'text/plain', "Content-Length" => '13'},
-        ["404 Not Found"]
+        {"Content-Type" => 'text/html', "Content-Length" => '48'},
+        ["<html><body><h4>404 Not Found</h4></body></html>"]
       ]
     end
   end
